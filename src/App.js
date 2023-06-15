1de22977c796b42a5a5a1eb89aa2e309c1de8e03
karu <rodrigo.rln.04@gmail.com>
2023-06-14 19:35:12 -0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import MenuInicio from './MenuInicio';
+import {Route, Routes} from 'react-router-dom'
+import { Connect4 } from './connect_4/C4';
 
-function App() {
+function App() { 
   return (
     <div className="App">
-      <MenuInicio />
+      <Routes>
+        <Route path="/" element= {<MenuInicio />}></Route>
+        <Route path="/Connect-4" element= {<Connect4 />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/MenuInicio.js
+++ src/MenuInicio.js
@@ -9,10 +9,12 @@ function MenuInicio() {
     <div className="menu-inicio">
       <h1 className="titulo-pagina">LU2PATHY</h1>
       <div className="botones-juego">
+        <a href="/connect_4/_app.js">
         <button className="cuadrado">
           <div className="nombre-juego">4 en línea</div>
           <img className="imagen-juego" src = {connect4IMG} alt="4 en línea" />
         </button>
+        </a>
         <button className="cuadrado">
           <div className="nombre-juego">Buscaminas</div>
           <img className="imagen-juego" src={minesweeperIMG} alt="Buscaminas" />
